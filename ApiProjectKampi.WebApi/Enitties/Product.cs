ece82a3c77f3146b9d3edbb518de3cc33fa8278d
Baran Günay <baranngunay@gmail.com>
2025-02-26 14:38:27 +0300

    ProjectStart - Entity Creates

---
+++ ApiProjectKampi.WebApi/Enitties/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjectKampi.WebApi.Enitties
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public Decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
